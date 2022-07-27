6032d152e74d53b446ece105c39889519a89c3ff
NoOourAn <nouranaborwash@gmail.com>
2022-07-27 22:15:47 +0200

    add tsc watch

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,4 @@
-export * from './ping.controller';
-export * from './hello.controller';
 export * from './customer.controller';
 export * from './film.controller';
 export * from './rental.controller';
+
